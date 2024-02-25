7f240d1aad73f592aa8601eaedb92fba3777cf81
Noxvill <gmo_mmo@live.cl>
2024-02-25 19:28:30 -0300

    mas cambios

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import PhotosProvider from "./Context/PhotosContext.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <PhotosProvider>
+        <App />
+      </PhotosProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
